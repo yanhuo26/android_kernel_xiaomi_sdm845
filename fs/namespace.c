3736c34e6c09a442c017bc4a9af0696621a32cac
CZB666-wdnmd <72345305+CZB666-wdnmd@users.noreply.github.com>
2024-04-19 00:45:25 +0000

    Revert "UMOUNT For Non GKI 4.4+ Kernels by @wxt1221"
    This reverts commit 578fdd571e87610a62970b7e8bd63a4110b3a45e.

--- fs/namespace.c
+++ fs/namespace.c
@@ -797,7 +797,7 @@ static void put_mountpoint(struct mountpoint *mp)
 	}
 }
 
-inline int check_mnt(struct mount *mnt)
+static inline int check_mnt(struct mount *mnt)
 {
 	return mnt->mnt_ns == current->nsproxy->mnt_ns;
 }
@@ -1170,7 +1170,7 @@ void flush_delayed_mntput_wait(void)
 	flush_delayed_work(&delayed_mntput_work);
 }
 
-void mntput_no_expire(struct mount *mnt)
+static void mntput_no_expire(struct mount *mnt)
 {
 	rcu_read_lock();
 	if (likely(READ_ONCE(mnt->mnt_ns))) {
@@ -1550,7 +1550,7 @@ static void umount_tree(struct mount *mnt, enum umount_tree_flags how)
 
 static void shrink_submounts(struct mount *mnt);
 
-int do_umount(struct mount *mnt, int flags)
+static int do_umount(struct mount *mnt, int flags)
 {
 	struct super_block *sb = mnt->mnt.mnt_sb;
 	int retval;
@@ -1689,7 +1689,7 @@ out_unlock:
 /* 
  * Is the caller allowed to modify his namespace?
  */
-inline bool may_mount(void)
+static inline bool may_mount(void)
 {
 	return ns_capable(current->nsproxy->mnt_ns->user_ns, CAP_SYS_ADMIN);
 }
@@ -1710,39 +1710,6 @@ static inline bool may_mandlock(void)
 	return false;
 }
 #endif
-
-static int can_umount(const struct path *path, int flags)
-{
-	struct mount *mnt = real_mount(path->mnt);
-
-	if (!may_mount())
-		return -EPERM;
-	if (path->dentry != path->mnt->mnt_root)
-		return -EINVAL;
-	if (!check_mnt(mnt))
-		return -EINVAL;
-	if (mnt->mnt.mnt_flags & MNT_LOCKED) /* Check optimistically */
-		return -EINVAL;
-	if (flags & MNT_FORCE && !capable(CAP_SYS_ADMIN))
-		return -EPERM;
-	return 0;
-}
-
-// caller is responsible for flags being sane
-int path_umount(struct path *path, int flags)
-{
-	struct mount *mnt = real_mount(path->mnt);
-	int ret;
-
-	ret = can_umount(path, flags);
-	if (!ret)
-		ret = do_umount(mnt, flags);
-
-	/* we mustn't call path_put() as that would clear mnt_expiry_mark */
-	dput(path->dentry);
-	mntput_no_expire(mnt);
-	return ret;
-}
 
 /*
  * Now umount can handle mount points as well as block devices.
